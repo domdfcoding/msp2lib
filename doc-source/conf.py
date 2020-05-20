fe4c0828b36e2960c6d5fb25b931241ba0ffae46
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-05-20 13:20:12 +0100

    Rearranged manpage and documentation build tools

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -1,16 +1,124 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# This file is managed by `git_helper`. Don't edit it directly
+
 import os
+import re
 import sys
 
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
 
-from sphinx_shared import *
+from sphinx.locale import _
 
-version = __version__
-copyright = __copyright__
+from __pkginfo__ import __version__
+
+
+github_url = f"https://github.com/domdfcoding/msp2lib"
+
+rst_prolog = f""".. |pkgname| replace:: msp2lib
+.. |pkgname2| replace:: ``msp2lib``
+.. |browse_github| replace:: `Browse the GitHub Repository <{github_url}>`__
+.. |ghurl| replace:: {github_url}
+"""
+
+author = "Dominic Davis-Foster"
+project = "msp2lib"
+slug = re.sub(r'\W+', '-', project.lower())
+release = version = __version__
+copyright = "Copyright 2020 Dominic Davis-Foster"
+language = 'en'
+
+extensions = [
+		'sphinx.ext.intersphinx',
+		'sphinx.ext.autodoc',
+		'sphinx.ext.mathjax',
+		'sphinx.ext.viewcode',
+		'sphinxcontrib.httpdomain',
+
+		]
+
+templates_path = ['_templates']
+html_static_path = ['_static']
+source_suffix = '.rst'
+exclude_patterns = []
+
 master_doc = 'index'
+suppress_warnings = ['image.nonlocal_uri']
+pygments_style = 'default'
 
-# All other settings are in `sphinx_shared`
+intersphinx_mapping = {
+		'rtd': ('https://docs.readthedocs.io/en/latest/', None),
+		'sphinx': ('http://www.sphinx-doc.org/en/stable/', None),
+		'python': ('https://docs.python.org/3/', None),
+		"NumPy": ('https://numpy.org/doc/stable/', None),
+		"SciPy": ('https://docs.scipy.org/doc/scipy/reference', None),
+		"matplotlib": ('https://matplotlib.org', None),
+		"h5py": ('https://docs.h5py.org/en/latest/', None),
+		"Sphinx": ('https://www.sphinx-doc.org/en/stable/', None),
+		"Django": ('https://docs.djangoproject.com/en/dev/', 'https://docs.djangoproject.com/en/dev/_objects/'),
+		"sarge": ('https://sarge.readthedocs.io/en/latest/', None),
+		"attrs": ('https://www.attrs.org/en/stable/', None),
+
+		}
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+		'logo_only': False,  # True will show just the logo
+
+		}
+html_theme_path = ["../.."]
+# html_logo = "logo/pyms.png"
+html_show_sourcelink = False  # True will show link to source
+
+html_context = {
+		# Github Settings
+		"display_github": True,  # Integrate GitHub
+		"github_user": "domdfcoding",  # Username
+		"github_repo": "msp2lib",  # Repo name
+		"github_version": "master",  # Version
+		"conf_py_path": "/",  # Path in the checkout to the docs root
+		}
+
+htmlhelp_basename = slug
+
+latex_documents = [
+		('index', '{0}.tex'.format(slug), project, author, 'manual'),
+		]
+
+man_pages = [
+		('index', slug, project, [author], 1)
+		]
+
+texinfo_documents = [
+		('index', slug, project, author, slug, project, 'Miscellaneous'),
+		]
+
+
+# Extensions to theme docs
+def setup(app):
+	from sphinx.domains.python import PyField
+	from sphinx.util.docfields import Field
+
+	app.add_object_type(
+			'confval',
+			'confval',
+			objname='configuration value',
+			indextemplate='pair: %s; configuration value',
+			doc_field_types=[
+					PyField(
+							'type',
+							label=_('Type'),
+							has_arg=False,
+							names=('type',),
+							bodyrolename='class'
+							),
+					Field(
+							'default',
+							label=_('Default'),
+							has_arg=False,
+							names=('default',),
+							),
+					]
+			)
--- manpage-builder/conf.py
+++ manpage-builder/conf.py
@@ -3,23 +3,44 @@
 
 import os
 import sys
+import re
 
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
 
-from sphinx_shared import *
+from __pkginfo__ import __version__
 
-version = f"{modname} {__version__}"
 copyright = (
-		f"{__copyright__}. License LGPLv3+: GNU LGPL version 3 or later "
+		f"Copyright 2020 Dominic Davis-Foster. License LGPLv3+: GNU LGPL version 3 or later "
 		f"<http://gnu.org/licenses/lgpl.html>\n\n"
 		"This is free software: you are free to change and redistribute it under certain conditions.\n\n"
 		"There is NO WARRANTY, to the extent permitted by law."
 		)
 
+author = "Dominic Davis-Foster"
+project = "msp2lib"
+release = version = f"{project} {__version__}"
+language = 'en'
+slug = re.sub(r'\W+', '-', project.lower())
+
 master_doc = 'manpage'
-man_pages = [
-		('manpage', slug, modname, [author], 1)
+
+extensions = [
+		'sphinx.ext.intersphinx',
+		'sphinx.ext.autodoc',
+		'sphinx.ext.mathjax',
+		'sphinx.ext.viewcode',
+		'sphinxcontrib.httpdomain',
 		]
 
-# All other settings are in `sphinx_shared`
+source_suffix = '.rst'
+exclude_patterns = []
+
+html_context = {"conf_py_path": "/"}
+
+htmlhelp_basename = slug
+
+man_pages = [
+		(master_doc, slug, project, [author], 1)
+		]
+
