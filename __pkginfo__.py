a8c9c254efc04b19b69bf8ba08384153cbdb0824
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-17 13:25:11 +0100

    Limit supported platforms to Linux

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -91,7 +91,6 @@ classifiers = [
 		'Programming Language :: Python :: 3 :: Only',
 		'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
 		'Typing :: Typed',
-		'Operating System :: OS Independent',
 
 		]
 
