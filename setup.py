dce7025ba7535529bffad0eef89c9838ffe3319e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-23 20:32:21 +0000

    Don't build manpage with sdist

--- setup.py
+++ setup.py
@@ -2,10 +2,7 @@
 # This file is managed by 'repo_helper'. Don't edit it directly.
 
 # stdlib
-import os
-import pathlib
 import sys
-import warnings
 
 # 3rd party
 from setuptools import setup
@@ -15,12 +12,6 @@ sys.path.append('.')
 # this package
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
-if not pathlib.Path("msp2lib.1").is_file():
-	warnings.warn("manpage not found. Trying to build now.")
-	exit_code = os.system("./make_manpage.sh")
-	if exit_code:
-		sys.exit(exit_code)
-
 setup(
 		description="Convert an MSP file representing one or more Mass Spectra to a NIST MS Search user library.",
 		extras_require=extras_require,
@@ -28,5 +19,3 @@ setup(
 		py_modules=[],
 		version=__version__,
 		)
-
-# TODO: include manpage in wheel and put in right place on filesystem
